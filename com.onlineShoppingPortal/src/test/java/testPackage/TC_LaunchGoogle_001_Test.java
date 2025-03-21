c48b0aa54ddf45a8983fa2611206cca959166892
admin <admin@Rohini-Shingare>
2025-03-21 12:10:30 +0530

    Second commit version 0.2

---
+++ com.onlineShoppingPortal/src/test/java/testPackage/TC_LaunchGoogle_001_Test.java
@@ -0,0 +1,17 @@
+package testPackage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_LaunchGoogle_001_Test {
+  @Test
+  public void LaunchGoogle() {
+	  WebDriver driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+	  driver.get("https://www.google.com/");
+  }
+}
---
+++ com.onlineShoppingPortal/src/test/java/testPackage/TC_LaunchJawaMotors_002_Test.java
@@ -0,0 +1,19 @@
+package testPackage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_LaunchJawaMotors_002_Test {
+  @Test
+  public void LaunchJawa() throws InterruptedException {
+	  WebDriver driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+	  driver.get("https://jawayezdistore.com/");
+	  Thread.sleep(3000);
+	  driver.quit();
+	  }
+}
